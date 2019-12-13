376f9fbb0ab68e7e2c281d0e50d892c77080d638
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2019-12-13 17:06:34 -0300

    Configurando a estrutura do template

---
+++ README.md
@@ -0,0 +1,2 @@
+Template heitorh3 Basic
+Template para projetos em React, com Estrutura de Pastas customizada, Estilos Globais, ESLint + Prettier, React Router e Axios configurados.
---
+++ template/src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Container from '../../Components/Container';
+
+export default function Main() {
+    return (
+        <Container>
+            <h1>Hello world!</h1>
+        </Container>
+    );
+}
